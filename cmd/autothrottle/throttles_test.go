cf8489776e806663df2592733da2de5ec7ba174d
Jamie Alquiza <jamiealquiza@gmail.com>
2018-03-07 15:09:48 -0700

    repCapacityByMetrics tests

--- cmd/autothrottle/throttles_test.go
+++ cmd/autothrottle/throttles_test.go
@@ -183,6 +183,20 @@ func TestRepCapacityByMetrics(t *testing.T) {
 	if curr != 80.00 {
 		t.Errorf("Expected current capacity of 80.00, got %.2f", curr)
 	}
+
+	// Test with missing instance type.
+	delete(rtm.limits, "mock")
+	_, _, _, err := repCapacityByMetrics(rtm, bmb, bm)
+	if err.Error() != "Unknown instance type" {
+		t.Errorf("Expected error 'Unknown instance type', got '%s'", err.Error())
+	}
+
+	// Test with a missing broker in the broker metrics.
+	delete(bm, 1004)
+	_, _, _, err = repCapacityByMetrics(rtm, bmb, bm)
+	if err.Error() != "Broker 1004 not found in broker metrics" {
+		t.Errorf("Expected error 'Broker 1004 not found in broker metrics', got '%s'", err.Error())
+	}
 }
 
 // func TestApplyTopicThrottles(t *testing.T) {}
