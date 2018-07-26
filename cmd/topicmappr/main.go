445b0641f9ba7804b0d790b06dc79d7f864ab18f
Jamie Alquiza <jamiealquiza@gmail.com>
2018-07-26 16:56:53 -0600

    cleanup

--- cmd/topicmappr/main.go
+++ cmd/topicmappr/main.go
@@ -15,10 +15,6 @@ import (
 	"github.com/jamiealquiza/envy"
 )
 
-const (
-	indent = "  "
-)
-
 var (
 	// Characters allowed in Kafka topic names
 	topicNormalChar, _ = regexp.Compile(`[a-zA-Z0-9_\\-]`)
@@ -50,7 +46,7 @@ func init() {
 
 	flag.StringVar(&Config.rebuildMap, "rebuild-map", "", "Rebuild a topic map")
 	topics := flag.String("rebuild-topics", "", "Rebuild topics (comma delim list) by lookup in ZooKeeper")
-	flag.BoolVar(&Config.useMeta, "use-meta", true, "Use broker Metadata as constraints")
+	flag.BoolVar(&Config.useMeta, "use-meta", true, "Use broker metadata in placement constraints")
 	flag.StringVar(&Config.zkAddr, "zk-addr", "localhost:2181", "ZooKeeper connect string (for broker Metadata or rebuild-topic lookups)")
 	flag.StringVar(&Config.zkPrefix, "zk-prefix", "", "ZooKeeper namespace prefix (for Kafka)")
 	flag.StringVar(&Config.zkMetricsPrefix, "zk-metrics-prefix", "topicmappr", "ZooKeeper namespace prefix (for Kafka metrics)")
@@ -146,175 +142,38 @@ func main() {
 	// Build a topic map with either
 	// text input or by fetching the
 	// map data from ZooKeeper.
+	// Store a copy of the original.
 	partitionMapIn := getPartitionMap(zk)
-
-	// Store a copy of the
-	// original map.
 	originalMap := partitionMapIn.Copy()
 
 	// Get a list of affected topics.
-	topics := topics(partitionMapIn)
+	getTopics(partitionMapIn)
 
-	fmt.Printf("\nTopics:\n")
-	for t := range topics {
-		fmt.Printf("%s%s\n", indent, t)
-	}
-
-	fmt.Printf("\nBroker change summary:\n")
-
-	// Get a broker map of the brokers in the current topic map.
-	// If meta data isn't being looked up, brokerMeta will be empty.
-	brokers := kafkazk.BrokerMapFromTopicMap(partitionMapIn, brokerMeta, Config.forceRebuild)
-
-	// Update the currentBrokers list with
-	// the provided broker list.
-	// TODO the information output of broker changes
-	// comes from within this Update call. Should return
-	// this info as a value and print it out here.
-	bs := brokers.Update(Config.brokers, brokerMeta)
-	change := bs.New - bs.Replace
-
-	// Store a copy.
-	brokersOrig := brokers.Copy()
+	brokers, brokersOrig, bs := getBrokers(partitionMapIn, brokerMeta)
 
 	// Check if any referenced brokers are
-	// marked as having missing metrics.
-	if Config.useMeta {
-		for id, b := range brokers {
-			// Missing brokers won't even
-			// be found in the brokerMeta.
-			if !b.Missing && id != 0 && brokerMeta[id].MetricsIncomplete {
-				fmt.Printf("Metrics not found for broker %d\n", id)
-				os.Exit(1)
-			}
-		}
-	}
+	// marked as having missing/partial
+	// metrics data.
+	ensureBrokerMetrics(brokers, brokerMeta)
 
-	// Get substitution affinities.
-	var affinities kafkazk.SubstitutionAffinities
-	if Config.subAffinity && !Config.forceRebuild {
-		var err error
-		affinities, err = brokers.SubstitutionAffinities(partitionMapIn)
-		if err != nil {
-			fmt.Printf("Substitution affinity error: %s\n", err.Error())
-			os.Exit(1)
-		}
-	}
+	// Create substitution affinities.
+	affinities := getSubAffinities(brokers, brokersOrig, partitionMapIn)
 
-	// Print substitution affinities.
-	if affinities != nil {
-		fmt.Printf("%s-\n", indent)
-	}
+	// Print changes, actions.
+	printChangesActions(bs)
 
-	for a, b := range affinities {
-		var inferred string
-		if brokersOrig[a].Missing {
-			inferred = "(inferred)"
-		}
-		fmt.Printf("%sSubstitution affinity: %d -> %d %s\n", indent, a, b.ID, inferred)
-	}
-
-	if bs.Changes() {
-		fmt.Printf("%s-\n", indent)
-	}
-
-	// Print change summary.
-	fmt.Printf("%sReplacing %d, added %d, missing %d, total count changed by %d\n",
-		indent, bs.Replace, bs.New, bs.Missing+bs.OldMissing, change)
-
-	// Print action.
-	fmt.Printf("\nAction:\n")
-
-	switch {
-	case change >= 0 && bs.Replace > 0:
-		fmt.Printf("%sRebuild topic with %d broker(s) marked for replacement\n",
-			indent, bs.Replace)
-	case change > 0 && bs.Replace == 0:
-		fmt.Printf("%sExpanding/rebalancing topic with %d additional broker(s) (this is a no-op unless --force-rebuild is specified)\n",
-			indent, bs.New)
-	case change < 0:
-		fmt.Printf("%sShrinking topic by %d broker(s)\n",
-			indent, -change)
-	case Config.replication == 0:
-		fmt.Printf("%sno-op\n", indent)
-	}
-
-	// If the replication factor is changed,
-	// the partition map input needs to have stub
-	// brokers appended (r factor increase) or
-	// existing brokers removed (r factor decrease).
-	if Config.replication > 0 {
-		fmt.Printf("%sUpdating replication factor to %d\n",
-			indent, Config.replication)
-
-		partitionMapIn.SetReplication(Config.replication)
-	}
+	// Apply any replication factor settings.
+	updateReplicationFactor(partitionMapIn)
 
 	// Build a new map using the provided list of brokers.
 	// This is ok to run even when a no-op is intended.
-
-	var partitionMapOut *kafkazk.PartitionMap
-	var warns []string
-
-	rebuildParams := kafkazk.RebuildParams{
-		PMM:          partitionMeta,
-		BM:           brokers,
-		Strategy:     Config.placement,
-		Optimization: Config.optimize,
-	}
-
-	if affinities != nil {
-		rebuildParams.Affinities = affinities
-	}
-
-	// If we're doing a force rebuild, the input map
-	// must have all brokers stripped out.
-	// A few notes about doing force rebuilds:
-	//	- Map rebuilds should always be called on a stripped PartitionMap copy.
-	//  - The BrokerMap provided in the Rebuild call should have
-	//		been built from the original PartitionMap, not the stripped map.
-	//  - A force rebuild assumes that all partitions will be lifted from
-	// 		all brokers and repositioned. This means you should call the
-	// 		SubStorageAll method on the BrokerMap if we're doing a "storage" placement strategy.
-	//		The SubStorageAll takes a PartitionMap and PartitionMetaMap. The PartitionMap is
-	// 		used to find partition to broker relationships so that the storage used can
-	//		be readded to the broker's StorageFree value. The amount to be readded, the
-	//		size of the partition, is referenced from the PartitionMetaMap.
-
-	if Config.forceRebuild {
-		// Get a stripped map that we'll call rebuild on.
-		partitionMapInStripped := partitionMapIn.Strip()
-		// If the storage placement strategy is being used,
-		// update the broker StorageFree values.
-		if Config.placement == "storage" {
-			err := rebuildParams.BM.SubStorageAll(partitionMapIn, partitionMeta)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		}
-
-		// Rebuild.
-		partitionMapOut, warns = partitionMapInStripped.Rebuild(rebuildParams)
-	} else {
-		// Update the StorageFree only on brokers
-		// marked for replacement.
-		if Config.placement == "storage" {
-			err := rebuildParams.BM.SubStorageReplacements(partitionMapIn, partitionMeta)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		}
-
-		// Rebuild directly on the input map.
-		partitionMapOut, warns = partitionMapIn.Rebuild(rebuildParams)
-	}
+	partitionMapOut, warns := buildMap(partitionMapIn, partitionMeta, brokers, affinities)
 
 	// Sort by topic, partition.
 	// TODO all functions should return
 	// standard lex sorted partition maps.
-	// This could probably be removed.
+	// Review for removal. Also, partitionMapIn
+	// shouldn't be further referenced.
 	sort.Sort(partitionMapIn.Partitions)
 	sort.Sort(partitionMapOut.Partitions)
 
@@ -324,7 +183,7 @@ func main() {
 		warns = append(warns, w)
 	}
 
-	// Print advisory warnings.
+	// Print warnings.
 	fmt.Println("\nWARN:")
 	if len(warns) > 0 {
 		sort.Strings(warns)
@@ -335,260 +194,18 @@ func main() {
 		fmt.Printf("%s[none]\n", indent)
 	}
 
-	// Ensure the topic name and partition
-	// order match.
-	for i := range originalMap.Partitions {
-		t1, t2 := originalMap.Partitions[i].Topic, partitionMapOut.Partitions[i].Topic
-		p1, p2 := originalMap.Partitions[i].Partition, partitionMapOut.Partitions[i].Partition
-		if t1 != t2 || p1 != p2 {
-			fmt.Println("Unexpected partition map order")
-			os.Exit(1)
-		}
-	}
+	// Print map change results.
+	printMapChanges(originalMap, partitionMapOut)
 
-	// Get a status string of what's changed.
-	fmt.Println("\nPartition map changes:")
-	for i := range originalMap.Partitions {
-		change := kafkazk.WhatChanged(originalMap.Partitions[i].Replicas,
-			partitionMapOut.Partitions[i].Replicas)
+	// Print broker assignment statistics.
+	printBrokerAssignmentStats(originalMap, partitionMapOut, brokersOrig, brokers)
 
-		fmt.Printf("%s%s p%d: %v -> %v %s\n",
-			indent,
-			originalMap.Partitions[i].Topic,
-			originalMap.Partitions[i].Partition,
-			originalMap.Partitions[i].Replicas,
-			partitionMapOut.Partitions[i].Replicas,
-			change)
-	}
-
-	// Write broker assignment statistics.
-	fmt.Println("\nBroker distribution:")
-
-	// Get general info.
-	dd1, dd2 := originalMap.DegreeDistribution().Stats(), partitionMapOut.DegreeDistribution().Stats()
-	fmt.Printf("%sdegree [min/max/avg]: %.0f/%.0f/%.2f -> %.0f/%.0f/%.2f\n",
-		indent, dd1.Min, dd1.Max, dd1.Avg, dd2.Min, dd2.Max, dd2.Avg)
-
-	fmt.Printf("%s-\n", indent)
-
-	// Per-broker info.
-	UseStats := partitionMapOut.UseStats()
-	for _, use := range UseStats {
-		fmt.Printf("%sBroker %d - leader: %d, follower: %d, total: %d\n",
-			indent, use.ID, use.Leader, use.Follower, use.Leader+use.Follower)
-	}
-
-	// Broker statistics.
-	if Config.verbose {
-		// fmt.Println("\nBroker statistics:")
-	}
-
-	// If we're using the storage placement strategy,
-	// write anticipated storage changes.
-	var div = 1073741824.00 // Fixed on GB for now.
-	if Config.placement == "storage" {
-		fmt.Println("\nStorage free change estimations:")
-
-		// Get filtered BrokerMaps. For the 'before' broker statistics, we want
-		// all brokers in the original BrokerMap that were also in the input PartitionMap.
-		// For the 'after' broker statistics, we want brokers that were not marked
-		// for replacement. We don't necessarily want to exclude brokers in the output
-		// that aren't mapped in the output PartitionMap. It's possible that a broker is
-		// not mapped to any of the input topics but is still holding data for other topics.
-		// It's ideal to still include that broker's storage metrics since it was a provided
-		// input and wasn't marked for replacement (generally, users are doing storage placements
-		// particularly to balance out the storage of the input broker list).
-		mb1, mb2 := brokersOrig.MappedBrokers(originalMap), brokers.NonReplacedBrokers()
-
-		// Range spread before/after.
-		rs1, rs2 := mb1.StorageRangeSpread(), mb2.StorageRangeSpread()
-		fmt.Printf("%srange spread: %.2f%% -> %.2f%%\n", indent, rs1, rs2)
-
-		// Std dev before/after.
-		sd1, sd2 := mb1.StorageStdDev(), mb2.StorageStdDev()
-		fmt.Printf("%sstd. deviation: %.2fGB -> %.2fGB\n", indent, sd1/div, sd2/div)
-
-		fmt.Printf("%s-\n", indent)
-
-		// Get changes in storage utilization.
-		storageDiffs := brokersOrig.StorageDiff(brokers)
-
-		// Pop IDs into a slice for sorted ouptut.
-		ids := []int{}
-		for id := range storageDiffs {
-			ids = append(ids, id)
-		}
-
-		sort.Ints(ids)
-
-		for _, id := range ids {
-			// Skip the internal reserved ID.
-			if id == 0 {
-				continue
-			}
-
-			diff := storageDiffs[id]
-
-			// Indicate if the broker
-			// is a replacement.
-			var replace string
-			if brokers[id].Replace {
-				replace = "*marked for replacement"
-			}
-
-			originalStorage := brokersOrig[id].StorageFree / div
-			newStorage := brokers[id].StorageFree / div
-			fmt.Printf("%sBroker %d: %.2f -> %.2f (%+.2fGB, %.2f%%) %s\n",
-				indent, id, originalStorage, newStorage, diff[0]/div, diff[1], replace)
-		}
-	}
-
+	// If no warnings were encountered, write out the
+	// output topic map(s).
 	if !Config.ignoreWarns && len(warns) > 0 {
 		fmt.Printf("\n%sWarnings encountered, partition map not created. Override with --ignore-warns.\n", indent)
 		os.Exit(1)
 	}
 
-	// Map per topic.
-	tm := map[string]*kafkazk.PartitionMap{}
-	for _, p := range partitionMapOut.Partitions {
-		if tm[p.Topic] == nil {
-			tm[p.Topic] = kafkazk.NewPartitionMap()
-		}
-		tm[p.Topic].Partitions = append(tm[p.Topic].Partitions, p)
-	}
-
-	fmt.Println("\nNew partition maps:")
-	// Global map if set.
-	if Config.outFile != "" {
-		err := kafkazk.WriteMap(partitionMapOut, Config.outPath+Config.outFile)
-		if err != nil {
-			fmt.Printf("%s%s", indent, err)
-		} else {
-			fmt.Printf("%s%s%s.json [combined map]\n", indent, Config.outPath, Config.outFile)
-		}
-	}
-
-	for t := range tm {
-		err := kafkazk.WriteMap(tm[t], Config.outPath+t)
-		if err != nil {
-			fmt.Printf("%s%s", indent, err)
-		} else {
-			fmt.Printf("%s%s%s.json\n", indent, Config.outPath, t)
-		}
-	}
-}
-
-func initZooKeeper() kafkazk.Handler {
-	if Config.useMeta || len(Config.rebuildTopics) > 0 || Config.placement == "storage" {
-		zk, err := kafkazk.NewHandler(&kafkazk.Config{
-			Connect:       Config.zkAddr,
-			Prefix:        Config.zkPrefix,
-			MetricsPrefix: Config.zkMetricsPrefix,
-		})
-		if err != nil {
-			fmt.Printf("Error connecting to ZooKeeper: %s\n", err)
-			os.Exit(1)
-		}
-
-		return zk
-	}
-
-	return nil
-}
-
-func getbrokerMeta(zk kafkazk.Handler) kafkazk.BrokerMetaMap {
-	if Config.useMeta {
-		// Whether or not we want to include
-		// additional broker metrics Metadata.
-		var withMetrics bool
-		if Config.placement == "storage" {
-			withMetrics = true
-		}
-
-		brokerMeta, errs := zk.GetAllBrokerMeta(withMetrics)
-		// If no data is returned, report and exit.
-		// Otherwise, it's possible that complete
-		// data for a few brokers wasn't returned.
-		// We check later whether any brokers that
-		// matter are missing metrics.
-		if errs != nil && brokerMeta == nil {
-			for _, e := range errs {
-				fmt.Println(e)
-			}
-			os.Exit(1)
-		}
-
-		return brokerMeta
-	}
-
-	return nil
-}
-
-func getPartitionMeta(zk kafkazk.Handler) kafkazk.PartitionMetaMap {
-	if Config.placement == "storage" {
-		partitionMeta, err := zk.GetAllPartitionMeta()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		return partitionMeta
-	}
-
-	return nil
-}
-
-func getPartitionMap(zk kafkazk.Handler) *kafkazk.PartitionMap {
-	switch {
-	// Provided as text.
-	case Config.rebuildMap != "":
-		pm, err := kafkazk.PartitionMapFromString(Config.rebuildMap)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		return pm
-	// Fetch from ZK.
-	case len(Config.rebuildTopics) > 0:
-		pm, err := kafkazk.PartitionMapFromZK(Config.rebuildTopics, zk)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		return pm
-	}
-
-	return nil
-}
-
-func topics(pm *kafkazk.PartitionMap) map[string]interface{} {
-	topics := map[string]interface{}{}
-	for _, p := range pm.Partitions {
-		topics[p.Topic] = nil
-	}
-
-	return topics
-}
-
-// containsRegex takes a topic name
-// reference and returns whether or not
-// it should be interpreted as regex.
-func containsRegex(t string) bool {
-	// Check each character of the
-	// topic name. If it doesn't contain
-	// a legal Kafka topic name character, we're
-	// going to assume it's regex.
-	for _, c := range t {
-		if !topicNormalChar.MatchString(string(c)) {
-			return true
-		}
-	}
-
-	return false
-}
-
-func defaultsAndExit() {
-	fmt.Println()
-	flag.PrintDefaults()
-	os.Exit(1)
+	writeMaps(partitionMapOut)
 }
