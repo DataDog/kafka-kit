8cfdb1b0d1222a1cd68c0a57ce181e75c84d1bf3
Jamie Alquiza <jamiealquiza@gmail.com>
2018-07-27 09:28:57 -0600

    README

--- cmd/topicmappr/main.go
+++ cmd/topicmappr/main.go
@@ -42,24 +42,24 @@ var (
 )
 
 func init() {
+	// Suppress underlying ZK client noise.
 	log.SetOutput(ioutil.Discard)
 
-	flag.StringVar(&Config.rebuildMap, "rebuild-map", "", "Rebuild a partition map")
-	topics := flag.String("rebuild-topics", "", "Rebuild topics (comma delim list) by lookup in ZooKeeper")
+	flag.StringVar(&Config.rebuildMap, "rebuild-map", "", "Rebuild a partition map provided as a string literal")
+	topics := flag.String("rebuild-topics", "", "Rebuild topics (comma delim. list) by lookup in ZooKeeper")
 	flag.BoolVar(&Config.useMeta, "use-meta", true, "Use broker metadata in placement constraints")
-	flag.StringVar(&Config.zkAddr, "zk-addr", "localhost:2181", "ZooKeeper connect string (for broker Metadata or rebuild-topic lookups)")
-	flag.StringVar(&Config.zkPrefix, "zk-prefix", "", "ZooKeeper namespace prefix (for Kafka)")
+	flag.StringVar(&Config.zkAddr, "zk-addr", "localhost:2181", "ZooKeeper connect string (for broker metadata or rebuild-topic lookups)")
+	flag.StringVar(&Config.zkPrefix, "zk-prefix", "", "ZooKeeper namespace prefix (for Kafka brokers)")
 	flag.StringVar(&Config.zkMetricsPrefix, "zk-metrics-prefix", "topicmappr", "ZooKeeper namespace prefix (for Kafka metrics)")
 	flag.StringVar(&Config.outPath, "out-path", "", "Path to write output map files to")
 	flag.StringVar(&Config.outFile, "out-file", "", "If defined, write a combined map of all topics to a file")
-	flag.BoolVar(&Config.ignoreWarns, "ignore-warns", false, "Whether a map should be produced if warnings are emitted")
-	flag.BoolVar(&Config.forceRebuild, "force-rebuild", false, "Forces a rebuild even if all existing brokers are provided")
-	flag.IntVar(&Config.replication, "replication", 0, "Set the replication factor")
+	flag.BoolVar(&Config.ignoreWarns, "ignore-warns", false, "Produce a map even if warnings are encountered")
+	flag.BoolVar(&Config.forceRebuild, "force-rebuild", false, "Forces a complete map rebuild")
+	flag.IntVar(&Config.replication, "replication", 0, "Normalize the topic replication factor across all replica sets")
 	flag.BoolVar(&Config.subAffinity, "sub-affinity", false, "Replacement broker substitution affinity")
-	flag.StringVar(&Config.placement, "placement", "count", "Partition placement type: [count, storage]")
-	flag.StringVar(&Config.optimize, "optimize", "distribution", "Optimization priority for storage placement: [distribution, storage]")
-	flag.BoolVar(&Config.verbose, "verbose", false, "Verbose information")
-	brokers := flag.String("brokers", "", "Broker list to rebuild topic partition map with")
+	flag.StringVar(&Config.placement, "placement", "count", "Partition placement strategy: [count, storage]")
+	flag.StringVar(&Config.optimize, "optimize", "distribution", "Optimization priority for the storage placement strategy: [distribution, storage]")
+	brokers := flag.String("brokers", "", "Broker list to scope all partition placements to")
 
 	envy.Parse("TOPICMAPPR")
 	flag.Parse()
@@ -167,9 +167,8 @@ func main() {
 	partitionMapOut, warns := buildMap(partitionMapIn, partitionMeta, brokers, affinities)
 
 	// Sort by topic, partition.
-	// TODO all functions should return lex sorted partition
-	// maps. Review for removal. Also, partitionMapIn
-	// shouldn't be further referenced from this point.
+	// TODO all functions should return lex sorted partition maps. Review for
+	// removal. Also, partitionMapIn shouldn't be further referenced at this point.
 	sort.Sort(partitionMapIn.Partitions)
 	sort.Sort(partitionMapOut.Partitions)
 
@@ -196,8 +195,7 @@ func main() {
 	// Print broker assignment statistics.
 	printBrokerAssignmentStats(originalMap, partitionMapOut, brokersOrig, brokers)
 
-	// If no warnings were encountered, write out the
-	// output partition map(s).
+	// If no warnings were encountered, write out the output partition map(s).
 	if !Config.ignoreWarns && len(warns) > 0 {
 		fmt.Printf("\n%sWarnings encountered, partition map not created. Override with --ignore-warns.\n", indent)
 		os.Exit(1)
