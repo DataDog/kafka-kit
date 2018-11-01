35fb6b9739a348a405b67e2292dfb4316efe5213
Jamie Alquiza <jamiealquiza@gmail.com>
2018-10-31 22:55:07 -0400

    init rebalance command

---
+++ cmd/topicmappr/commands/rebalance.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rebalanceCmd = &cobra.Command{
+	Use:   "rebalance",
+	Short: "Rebalance a partition map for one or more topics",
+	Long:  `Rebalance`,
+	Run:   rebalance,
+}
+
+func init() {
+	rootCmd.AddCommand(rebalanceCmd)
+
+	rebalanceCmd.Flags().String("topics", "", "Rebuild topics (comma delim. list) by lookup in ZooKeeper")
+	rebalanceCmd.Flags().String("out-path", "", "Path to write output map files to")
+	rebalanceCmd.Flags().String("out-file", "", "If defined, write a combined map of all topics to a file")
+	rebalanceCmd.Flags().String("brokers", "", "Broker list to scope all partition placements to")
+	rebalanceCmd.Flags().String("zk-metrics-prefix", "topicmappr", "ZooKeeper namespace prefix for Kafka metrics (when using storage placement)")
+
+	// Required.
+	rebalanceCmd.MarkFlagRequired("brokers")
+}
+
+func rebalance(cmd *cobra.Command, _ []string) {
+	bootstrap(cmd)
+
+	// ZooKeeper init.
+	zk, err := initZooKeeper(cmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer zk.Close()
+}
