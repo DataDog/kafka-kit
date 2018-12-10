8945a1300db30a7e36d0a4bf161195dd9b79f4a5
Jamie Alquiza <jamiealquiza@gmail.com>
2018-12-10 15:47:57 -0700

    request rate limiter implementation

---
+++ registry/server/throttle.go
@@ -0,0 +1,69 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+var (
+	ErrRequestThrottleTimeout = errors.New("wait time exceeded")
+)
+
+// RequestThrottle controls request rates with
+// a configurable burst capacity and per-second rate
+// backed with a token bucket.
+type RequestThrottle interface {
+	Request(context.Context) error
+}
+
+type requestThrottle struct {
+	c chan struct{}
+}
+
+// RequestThrottleConfig specifies the RequestThrottle
+// burst capacity and per-second rate limit.
+type RequestThrottleConfig struct {
+	// Burst capacity.
+	Capacity int
+	// Request rate (reqs/s).
+	Rate int
+}
+
+// NewRequestThrottle initializes a RequestThrottle.
+func NewRequestThrottle(cfg RequestThrottleConfig) (RequestThrottle, error) {
+	if cfg.Rate < 1 {
+		return nil, errors.New("rate must be >= 1")
+	}
+
+	t := &requestThrottle{
+		c: make(chan struct{}, cfg.Capacity),
+	}
+
+	d := time.Duration(1000000000 / cfg.Rate)
+
+	refill := time.NewTicker(d * time.Nanosecond)
+
+	// Bucket refill.
+	go func() {
+		for _ = range refill.C {
+			<-t.c
+		}
+	}()
+
+	return t, nil
+}
+
+// Request takes a context and attempts to acquire a token
+// from the requestThrottle. An error is returned if a free
+// token is not acquired by the context expiration.
+func (t *requestThrottle) Request(ctx context.Context) error {
+	select {
+	case t.c <- struct{}{}:
+		break
+	case <-ctx.Done():
+		return ErrRequestThrottleTimeout
+	}
+
+	return nil
+}
