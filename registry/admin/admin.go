e73441e27a445cab17625dae97f22b8d148eeeba
Jamie Alquiza <jamiealquiza@gmail.com>
2019-12-10 11:49:19 -0700

    adds ReplicaAssignment type

--- registry/admin/admin.go
+++ registry/admin/admin.go
@@ -10,18 +10,19 @@ type Client interface {
 	CreateTopic(context.Context, CreateTopicConfig) error
 }
 
+type Config struct {
+	Type string
+	// Kafka native admin configs.
+	BootstrapServers string
+}
+
 // CreateTopicConfig holds CreateTopic parameters.
 type CreateTopicConfig struct {
 	Name              string
 	Partitions        int
 	ReplicationFactor int
 	Config            map[string]string
-	ReplicaAssignment [][]int32
+	ReplicaAssignment ReplicaAssignment
 }
 
-type Config struct {
-	Type string
-
-	// Kafka native admin configs.
-	BootstrapServers string
-}
+type ReplicaAssignment [][]int32
