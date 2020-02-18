266e12b2402ea7831f0f0b998818118b4250f18f
Jamie Alquiza <jamiealquiza@gmail.com>
2020-02-18 16:11:21 -0700

    updates kafkametrics Broker fields

--- kafkametrics/kafkametrics.go
+++ kafkametrics/kafkametrics.go
@@ -1,30 +1,31 @@
-// Package kafkametrics fetches Kafka
-// broker metrics and posts events to
+// Package kafkametrics fetches Kafka broker metrics and posts events to
 // supported metrics backends.
 package kafkametrics
 
-// Handler requests broker metrics
-// and posts events.
+// Handler requests broker metrics and posts events.
 type Handler interface {
 	GetMetrics() (BrokerMetrics, []error)
 	PostEvent(*Event) error
 }
 
-// BrokerMetrics is a map of broker IDs
-// to *Broker structs.
+// BrokerMetrics is a map of broker IDs to *Broker structs.
 type BrokerMetrics map[int]*Broker
 
-// Broker holds metrics and metadata
-// for a Kafka broker.
+// Broker holds metrics and metadata for a Kafka broker.
 type Broker struct {
-	ID           int
-	Host         string
+	// Kafka broker ID.
+	ID int
+	// Kafka broker hostname.
+	Host string
+	// Kafka broker instance type.
 	InstanceType string
-	NetTX        float64
+	// Network tx, window avg.
+	NetTX float64
+	// Network rx, window avg.
+	NetRX float64
 }
 
-// Event is used to post autothrottle
-// events to the backend metrics system.
+// Event is used to post autothrottle events to the backend metrics system.
 type Event struct {
 	Title string
 	Text  string
