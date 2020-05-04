5260d068b8fae8fd7a36cad4e1984e92f1670507
Jamie Alquiza <jamiealquiza@gmail.com>
2020-05-04 17:18:07 -0600

    brokerIDFromPath

--- cmd/autothrottle/api_parsing.go
+++ cmd/autothrottle/api_parsing.go
@@ -39,9 +39,23 @@ func parseAutoRemoveParam(req *http.Request) (bool, error) {
 	return autoRemove, nil
 }
 
-func paths(req *http.Request) []string {
+func parsePaths(req *http.Request) []string {
 	urlPathTrimmed := strings.Trim(req.URL.Path, "/")
 	return strings.Split(urlPathTrimmed, "/")
+}
+
+func brokerIDFromPath(req *httpRequest) (int, error) {
+	paths := parsePaths(req)
+	if len(paths) < 2 {
+		return 0, errors.New("broker ID not provided\n")
+	}
+
+	id, err := strconv.Atoi(paths[1])
+	if err != nil {
+		return 0, errors.New("broker param must be provided as integer")
+	}
+
+	return id, nil
 }
 
 func logReq(req *http.Request) {
