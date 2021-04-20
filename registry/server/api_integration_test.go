597e9622839bb16a87298c34f1272ad87d26c611
Jamie Alquiza <jamiealquiza@gmail.com>
2021-04-19 21:38:47 -0400

    TestCreateTaggedTopic

--- registry/server/api_integration_test.go
+++ registry/server/api_integration_test.go
@@ -37,6 +37,41 @@ func TestCreateTopic(t *testing.T) {
 	}
 }
 
+func TestCreateTaggedTopic(t *testing.T) {
+	s, err := testIntegrationServer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Tag a broker so we can test partition mapping by tag.
+	_, err = s.TagBroker(context.Background(), &pb.BrokerRequest{Id: 1001, Tag: []string{"key:value"}})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tests := map[int]*pb.CreateTopicRequest{
+		0: &pb.CreateTopicRequest{
+			Topic:            &pb.Topic{Name: "new_topic2", Partitions: 1, Replication: 1},
+			TargetBrokerTags: []string{"key:value"},
+		},
+		1: &pb.CreateTopicRequest{
+			Topic:            &pb.Topic{Name: "new_topic3", Partitions: 1, Replication: 1},
+			TargetBrokerTags: []string{"key:doesnt_exist"}},
+	}
+
+	expectedErrors := map[int]error{
+		0: nil,
+		1: ErrInsufficientBrokers,
+	}
+
+	for i := 0; i < len(tests); i++ {
+		_, err := s.CreateTopic(context.Background(), tests[i])
+		if err != expectedErrors[i] {
+			t.Errorf("Expected error '%s' for test %d, got '%s'", expectedErrors[i], i, err)
+		}
+	}
+}
+
 func TestDeleteTopic(t *testing.T) {
 	s, err := testIntegrationServer()
 	if err != nil {
