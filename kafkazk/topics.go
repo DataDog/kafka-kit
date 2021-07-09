c3787bae5b683c1e169849ddb9b769d9ac45de42
Jamie Alquiza <jamiealquiza@gmail.com>
2021-07-09 16:00:17 -0400

    moves TopicState

--- kafkazk/topics.go
+++ kafkazk/topics.go
@@ -4,6 +4,12 @@ import (
 	"sort"
 )
 
+// TopicState is used for unmarshing ZooKeeper json data from a topic:
+// e.g. /brokers/topics/some-topic
+type TopicState struct {
+	Partitions map[string][]int `json:"partitions"`
+}
+
 // Brokers returns a []int of broker IDs from all partitions in the TopicState.
 func (ts *TopicState) Brokers() []int {
 	var IDs []int
