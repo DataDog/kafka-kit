62eb9834f7331647ee5d1ea11a36b1ecea4cfb6f
Jamie Alquiza <jamiealquiza@gmail.com>
2021-12-06 16:54:13 -0500

    NewZooKeeperLock, path init

--- cluster/zookeeper/zookeeper.go
+++ cluster/zookeeper/zookeeper.go
@@ -1,19 +1,50 @@
 package zookeeper
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
 	"github.com/go-zookeeper/zk"
 )
 
 type ZooKeeperLock struct {
-	c *zk.Conn
+	c    *zk.Conn
+	Path string
 }
 
-func NewZooKeeperLock() (ZooKeeperLock, error) {
-	return ZooKeeperLock{}, nil
+type ZooKeeperLockConfig struct {
+	Address string
+	Path    string
 }
 
-func NewZooKeeperLockWithClient(c *zk.Conn) (ZooKeeperLock, error) {
-	return ZooKeeperLock{c: c}, nil
+func NewZooKeeperLock(c ZooKeeperLockConfig) (ZooKeeperLock, error) {
+	var zkl = ZooKeeperLock{Path: c.Path}
+	var err error
+
+	zkl.c, _, err = zk.Connect([]string{c.Address}, 10*time.Second, zk.WithLogInfo(false))
+	if err != nil {
+		return zkl, err
+	}
+
+	return zkl, zkl.init()
+}
+
+func (z ZooKeeperLock) init() error {
+	// Get an incremental path ending at the destination locking path. If for
+	// example we're provided "/path/to/locks", we want the following:
+	// ["/path", "/path/to", "/path/to/locks"].
+	nodes := strings.Split(strings.Trim(z.Path, "/"), "/")
+
+	// Create each node.
+	for i := range nodes {
+		nodePath := fmt.Sprintf("/%s", strings.Join(nodes[:i+1], "/"))
+		if _, e := z.c.Create(nodePath, nil, 0, zk.WorldACL(31)); e != nil {
+			return e
+		}
+	}
+
+	return nil
 }
 
 func (z ZooKeeperLock) Lock() {
