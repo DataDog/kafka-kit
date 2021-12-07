1331e770ae1f30ce6ea5387f754c7c83c924cf17
Jamie Alquiza <jamiealquiza@gmail.com>
2021-12-07 16:11:17 -0500

    TestLockPath

--- cluster/zookeeper/locks_test.go
+++ cluster/zookeeper/locks_test.go
@@ -12,44 +12,41 @@ func TestIDs(t *testing.T) {
 	c := &mockZooKeeperClient{
 		znodeNameTemplate: "_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-00000000",
 		locks: []string{
-			"/locks/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000001",
-			"/locks/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000002",
+			"/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000001",
+			"/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000002",
 		},
 	}
 
 	lock := newMockZooKeeperLockWithClient(c)
-
 	locks, _ := lock.locks()
+
 	// Test IDs.
 	assert.ElementsMatch(t, locks.IDs(), []int{1, 2}, "Unexpected IDs list")
 }
 
-// func TestLockPath(t *testing.T) {
-//   client := &mockZooKeeperClient{
-//     znodeNameTemplate: "_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-00000000",
-//     locks:             []string{
-//       "/locks/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000001",
-//       "/locks/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000002"
-//     },
-//   },
-// 	lock := newMockZooKeeperLockWithClient(c)
-// 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-//
-//   locks.
-//
-//   locks, _ := lock.locks()
-//
-//   expectedIDs := []int{1,2}
-//   expectedLocks := map[string]string{
-//     "1": "/locks/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000001",
-//     "2": "/locks/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000002",
-//   }
-//
-//   // Test IDs.
-//   assert.ElementsMatch(t, locks.IDs(), expectedIDs, "Unexpected IDs list")
-//
-//   // Test ID to znode value.
-//   for id, expectedZnode := expectedLocks {
-//
-//   }
-// }
+func TestLockPath(t *testing.T) {
+	c := &mockZooKeeperClient{
+		znodeNameTemplate: "_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-00000000",
+		locks: []string{
+			"_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000001",
+			"_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000002",
+		},
+	}
+
+	lock := newMockZooKeeperLockWithClient(c)
+	locks, _ := lock.locks()
+
+	expectedLocks := map[int]string{
+		1: "/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000001",
+		2: "/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000002",
+	}
+
+	// Test ID to znode value.
+	for id, expectedZnode := range expectedLocks {
+		znode, err := locks.LockPath(id)
+		if err != nil {
+			t.Errorf("Unexepected error: %s", err)
+		}
+		assert.Equal(t, znode, expectedZnode, "incorrect znode")
+	}
+}
--- cluster/zookeeper/zookeeper_test.go
+++ cluster/zookeeper/zookeeper_test.go
@@ -46,8 +46,7 @@ func (m *mockZooKeeperClient) CreateProtectedEphemeralSequential(s string, b []b
 	// Mimic the sequential znode naming scheme. If s == "/locks/lock-", we want
 	// "/locks/_c_979cb11f40bb3dbc6908edeaac8f2de1-lock-000000001"
 	parts := strings.Split(s, "/")
-	fakeZnode := fmt.Sprintf("/%s%s%d", parts[1], m.znodeNameTemplate, atomic.AddInt32(&m.nextID, 1))
-
+	fakeZnode := fmt.Sprintf("/%s/%s%d", parts[1], m.znodeNameTemplate, atomic.AddInt32(&m.nextID, 1))
 	// Store the fake lock name.
 	m.locks = append(m.locks, fakeZnode)
 
