47b18440f59d50e125aee515378b143fb7606a44
Jamie Alquiza <jamiealquiza@gmail.com>
2022-02-11 12:53:47 -0500

    [kafkaadmin] interface updated for config

--- kafkaadmin/kafkaadmin.go
+++ kafkaadmin/kafkaadmin.go
@@ -14,7 +14,7 @@ type KafkaAdmin interface {
 	CreateTopic(context.Context, CreateTopicConfig) error
 	DeleteTopic(context.Context, string) error
 	// Cluster.
-	SetThrottle(context.Context, ThrottleConfig) error
+	SetThrottle(context.Context, SetThrottleConfig) error
 	RemoveThrottle(context.Context, RemoveThrottleConfig) error
 	GetDynamicConfigs(context.Context, string, []string) (ResourceConfigs, error)
 }
