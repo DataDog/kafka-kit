e371c1bb7e7862b6273d6ce78b3ac60b0c6512a1
Jamie Alquiza <jamiealquiza@gmail.com>
2022-02-22 16:21:11 -0500

    [kafkaadmin] TestClearTopicThrottleConfigs

--- kafkaadmin/throttles_test.go
+++ kafkaadmin/throttles_test.go
@@ -88,6 +88,64 @@ func TestPopulateTopicConfigs(t *testing.T) {
 	}
 }
 
-// func TestClearTopicThrottleConfigs(t *testing.T) {}
+func TestClearTopicThrottleConfigs(t *testing.T) {
+	tests := []struct {
+		input       ResourceConfigs
+		expected    ResourceConfigs
+		expectedErr error
+	}{
+		// Case: Empty.
+		{
+			input:       ResourceConfigs{},
+			expected:    ResourceConfigs{},
+			expectedErr: nil,
+		},
+		// Case: One topic only has configs unrelated to throttles and is excluded.
+		// Another topic has a throttle that needs to be unset.
+		{
+			input: ResourceConfigs{
+				"topic1": map[string]string{
+					"message.retention.ms": "10000",
+				},
+				"topic2": map[string]string{
+					"leader.replication.throttled.replicas": "0:1001",
+				},
+			},
+			expected: ResourceConfigs{
+				"topic2": map[string]string{},
+			},
+			expectedErr: nil,
+		},
+		// Case: Two topics with throttles that need to be cleared. One topic has
+		// unrelated configs that need to be retained.
+		{
+			input: ResourceConfigs{
+				"topic1": map[string]string{
+					"leader.replication.throttled.replicas": "*",
+				},
+				"topic2": map[string]string{
+					"follower.replication.throttled.replicas": "*",
+					"message.retention.ms":                    "10000",
+				},
+			},
+			expected: ResourceConfigs{
+				"topic1": map[string]string{},
+				"topic2": map[string]string{
+					"message.retention.ms": "10000",
+				},
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for i, testCase := range tests {
+		err := clearTopicThrottleConfigs(testCase.input)
+		// Check the error.
+		assert.Equalf(t, testCase.expectedErr, err, fmt.Sprintf("case %d", i))
+		// Check the output.
+		assert.Equalf(t, testCase.expected, testCase.input, fmt.Sprintf("case %d", i))
+	}
+}
+
 // func TestPopulateBrokerConfigs(t *testing.T) {}
 // func TestClearBrokerThrottleConfigs(t *testing.T) {}
