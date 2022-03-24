6ff80c7895b7440db362a3703e238b58e78ced10
Zeke Rosenberg <zeke.rosenberg@datadoghq.com>
2022-03-24 14:03:35 -0700

    Adds a new test case for chunk size greater than available brokers

--- cmd/topicmappr/commands/chunked_test.go
+++ cmd/topicmappr/commands/chunked_test.go
@@ -11,7 +11,7 @@ import (
 func TestBasicChunkedDownscale(t *testing.T) {
 	var inMap = readTestPartitionMap("nine_brokers.json")
 	var finalMap = readTestPartitionMap("three_brokers.json")
-	var chunks = getPartitionMapChunk(&finalMap, &inMap, kafkazk.BrokerList{
+	var chunks = getPartitionMapChunks(&finalMap, &inMap, kafkazk.BrokerList{
 		&kafkazk.Broker{ID: 10000},
 		&kafkazk.Broker{ID: 10001},
 		&kafkazk.Broker{ID: 10002},
@@ -25,12 +25,15 @@ func TestBasicChunkedDownscale(t *testing.T) {
 	validateFinalChunk(t, chunks, finalMap)
 	validateMapDoesNotContainBrokers(t, chunks[0], []int{10008, 10007, 10006})
 	validateMapDoesNotContainBrokers(t, chunks[1], []int{10003, 10004, 10005, 10006, 10007, 10008})
+	if len(chunks) != 2 {
+		t.Errorf("Removing 6 brokers in 2 chunks of 3, expected to return 2 map chunks.")
+	}
 }
 
 func TestIgnoresNoop(t *testing.T) {
 	var inMap = readTestPartitionMap("nine_brokers.json")
 	var finalMap = readTestPartitionMap("nine_brokers.json")
-	var chunks = getPartitionMapChunk(&finalMap, &inMap, kafkazk.BrokerList{
+	var chunks = getPartitionMapChunks(&finalMap, &inMap, kafkazk.BrokerList{
 		&kafkazk.Broker{ID: 10000},
 		&kafkazk.Broker{ID: 10001},
 		&kafkazk.Broker{ID: 10002},
@@ -49,7 +52,7 @@ func TestIgnoresNoop(t *testing.T) {
 func TestBasicChunkedDownscaleStepSizeOne(t *testing.T) {
 	var inMap = readTestPartitionMap("nine_brokers.json")
 	var finalMap = readTestPartitionMap("three_brokers.json")
-	var chunks = getPartitionMapChunk(&finalMap, &inMap, kafkazk.BrokerList{
+	var chunks = getPartitionMapChunks(&finalMap, &inMap, kafkazk.BrokerList{
 		&kafkazk.Broker{ID: 10000},
 		&kafkazk.Broker{ID: 10001},
 		&kafkazk.Broker{ID: 10002},
@@ -67,12 +70,15 @@ func TestBasicChunkedDownscaleStepSizeOne(t *testing.T) {
 	validateMapDoesNotContainBrokers(t, chunks[3], []int{10008, 10007, 10006, 10005})
 	validateMapDoesNotContainBrokers(t, chunks[4], []int{10008, 10007, 10006, 10005, 10004})
 	validateMapDoesNotContainBrokers(t, chunks[5], []int{10008, 10007, 10006, 10005, 10004, 10003})
+	if len(chunks) != 6 {
+		t.Errorf("Removing 6 brokers in 6 chunks of 1, expected to return 1 map chunk per broker removed.")
+	}
 }
 
 func TestChunksGeneratedWithConsistentOrdering(t *testing.T) {
 	var inMap = readTestPartitionMap("nine_brokers.json")
 	var finalMap = readTestPartitionMap("three_brokers.json")
-	var chunks = getPartitionMapChunk(&finalMap, &inMap, kafkazk.BrokerList{
+	var chunks = getPartitionMapChunks(&finalMap, &inMap, kafkazk.BrokerList{
 		&kafkazk.Broker{ID: 10001},
 		&kafkazk.Broker{ID: 10000},
 		&kafkazk.Broker{ID: 10006},
@@ -90,12 +96,15 @@ func TestChunksGeneratedWithConsistentOrdering(t *testing.T) {
 	validateMapDoesNotContainBrokers(t, chunks[3], []int{10008, 10007, 10006, 10005})
 	validateMapDoesNotContainBrokers(t, chunks[4], []int{10008, 10007, 10006, 10005, 10004})
 	validateMapDoesNotContainBrokers(t, chunks[5], []int{10008, 10007, 10006, 10005, 10004, 10003})
+	if len(chunks) != 6 {
+		t.Errorf("Removing 6 brokers in 6 chunks of 1, expected to return 1 map chunk per broker removed.")
+	}
 }
 
 func TestUnevenBrokersForStepSize(t *testing.T) {
 	var inMap = readTestPartitionMap("nine_brokers.json")
 	var finalMap = readTestPartitionMap("three_brokers.json")
-	var chunks = getPartitionMapChunk(&finalMap, &inMap, kafkazk.BrokerList{
+	var chunks = getPartitionMapChunks(&finalMap, &inMap, kafkazk.BrokerList{
 		&kafkazk.Broker{ID: 10000},
 		&kafkazk.Broker{ID: 10001},
 		&kafkazk.Broker{ID: 10002},
@@ -109,6 +118,30 @@ func TestUnevenBrokersForStepSize(t *testing.T) {
 	validateFinalChunk(t, chunks, finalMap)
 	validateMapDoesNotContainBrokers(t, chunks[0], []int{10005, 10006, 10007, 10008})
 	validateMapDoesNotContainBrokers(t, chunks[1], []int{10003, 10004, 10005, 10006, 10007, 10008})
+	if len(chunks) != 2 {
+		t.Errorf("Removing 6 brokers in 2 chunks of 4, expected to return only two map chunks.")
+	}
+}
+
+func TestChunkSizeBiggerThanAvailableBrokers(t *testing.T) {
+	var inMap = readTestPartitionMap("nine_brokers.json")
+	var finalMap = readTestPartitionMap("three_brokers.json")
+	var chunks = getPartitionMapChunks(&finalMap, &inMap, kafkazk.BrokerList{
+		&kafkazk.Broker{ID: 10000},
+		&kafkazk.Broker{ID: 10001},
+		&kafkazk.Broker{ID: 10002},
+		&kafkazk.Broker{ID: 10003},
+		&kafkazk.Broker{ID: 10004},
+		&kafkazk.Broker{ID: 10005},
+		&kafkazk.Broker{ID: 10006},
+		&kafkazk.Broker{ID: 10007},
+		&kafkazk.Broker{ID: 10008}}, 12)
+
+	validateFinalChunk(t, chunks, finalMap)
+	validateMapDoesNotContainBrokers(t, chunks[0], []int{10003, 10004, 10005, 10006, 10007, 10008})
+	if len(chunks) != 1 {
+		t.Errorf("Chunk size is bigger than available brokers, expected to return only the final map.")
+	}
 }
 
 func validateFinalChunk(t *testing.T, chunks []*kafkazk.PartitionMap, finalMap kafkazk.PartitionMap) {
